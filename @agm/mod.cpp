0cbfef7e1676006cacbc3a73fcb4adf66847612c
Jeppe Richardt <marci@marci.dk>
2015-02-08 03:11:14 +0100

    Added CBA, AGM & sthud(++)

---
+++ @agm/mod.cpp
@@ -0,0 +1,5 @@
+name = "Authentic Gameplay Mod";
+picture = "agm_logo.paa";
+actionName = "Github";
+action = "https://github.com/KoffeinFlummi/AGM";
+description = "Authentic Gameplay Mod - Version 0.95.2";
